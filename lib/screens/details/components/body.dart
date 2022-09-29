70c5a57f4ea27e431aa9219e22698e5f72bd57c7
IngNex <ingnexmaicol14@gmail.com>
2022-09-29 11:11:27 -0500

    add img in body details object

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:front_plant_app_flutter_ui/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -12,12 +15,34 @@ class Body extends StatelessWidget {
       children: [
         Row(
           children: [
-            Expanded(child: Column()),
+            Expanded(
+              child: Column(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: SvgPicture.asset('assets/icons/back_arrow.svg'),
+                  ),
+                ],
+              ),
+            ),
             Container(
               height: size.height * 0.8,
               width: size.width * 0.75,
               decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(60),
+                  bottomLeft: Radius.circular(60),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 60,
+                      color: nexPrimaryColor.withOpacity(0.29)),
+                ],
                 image: DecorationImage(
+                  alignment: Alignment.centerLeft,
                   fit: BoxFit.cover,
                   image: AssetImage('assets/images/img.png'),
                 ),
