fd58018488173d1877634a7c8433d10bdffcdf2b
IngNex <ingnexmaicol14@gmail.com>
2022-09-28 23:53:02 -0500

    added bottomNavCard

---
+++ lib/components/bottomNavBar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:front_plant_app_flutter_ui/constants.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          left: nexDefaultPadding,
+          right: nexDefaultPadding,
+          bottom: nexDefaultPadding),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -10),
+            blurRadius: 35,
+            color: nexPrimaryColor.withOpacity(0.38),
+          ),
+        ],
+      ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset('assets/icons/flower.svg'),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset('assets/icons/heart-icon.svg'),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset('assets/icons/user-icon.svg'),
+        ),
+      ]),
+    );
+  }
+}
--- lib/screens/components/home_screen.dart
+++ lib/screens/components/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:front_plant_app_flutter_ui/components/bottomNavBar.dart';
 import 'package:front_plant_app_flutter_ui/constants.dart';
 import 'package:front_plant_app_flutter_ui/screens/components/body.dart';
 
@@ -11,38 +12,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.only(
-            left: nexDefaultPadding,
-            right: nexDefaultPadding,
-            bottom: nexDefaultPadding),
-        height: 80,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, -10),
-              blurRadius: 35,
-              color: nexPrimaryColor.withOpacity(0.38),
-            ),
-          ],
-        ),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset('assets/icons/flower.svg'),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset('assets/icons/heart-icon.svg'),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset('assets/icons/user-icon.svg'),
-          ),
-        ]),
-      ),
+      bottomNavigationBar: BottomNavBar(),
     );
   }
 
