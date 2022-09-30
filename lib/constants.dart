474ca228784c597188967b3e538b83c6f7e7675f
IngNex <ingnexmaicol14@gmail.com>
2022-09-29 23:08:11 -0500

    Added colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //Colores que vamos usar para la app
-const nexPrimaryColor = Color(0xFF0C9889);
+const nexPrimaryColor = Color.fromARGB(255, 21, 146, 155);
 const nexTextColor = Color(0xFF3C4046);
 const nexBackgroundColor = Color(0xFFF9F8FD);
 
